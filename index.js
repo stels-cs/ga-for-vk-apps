fb46ca8b8c670835266e13c1c911e2e0860ca716
Иван Недзвецкий <stels-cs@yandex.ru>
2017-02-19 19:13:16 +0300

    Fix double requests

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ import {GA} from './src/index'
 
 GA.create('UA-67358648-1')
 GA.pageview()
+GA.event('test', 'event', 'l_event')
--- src/ImageSender.js
+++ src/ImageSender.js
@@ -8,6 +8,10 @@ export default class ImageSender extends Sender {
     constructor(image) {
         super()
         this._image = image
+        this._image.onload = () => { this._next() }
+        this._image.onerror = () => { this._next() }
+        this._lock = false
+        this._queue = []
         this._z = (new Date()).getTime()
         this._image.width = 1
         this._image.height = 1
@@ -23,10 +27,27 @@ export default class ImageSender extends Sender {
                 s += key + "=" + data[key] + "&"
         }
         s += "z=" + (this._getZ())
-        this._image.src = s
+        this._upload(s)
     }
     
     _getZ() {
         return this._z++
+    }
+
+    _next() {
+        this._lock = false
+        let src = this._queue.shift()
+        if (src) {
+            this._upload(src)
+        }
+    }
+
+    _upload(src) {
+        if (this._lock) {
+            this._queue.push(src)
+        } else {
+            this._lock = true
+            this._image.src = src
+        }
     }
 }
